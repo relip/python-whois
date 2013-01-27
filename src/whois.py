51a108d7d5f55cf1181c97cbe5e93731d9911d12
Larry Kim <admin@relip.org>
2013-01-28 03:00:32 +0900

    Change directory structure

--- src/whois.py
+++ src/whois.py
@@ -9,18 +9,26 @@ class Whois(object):
 		self.domain = domain
 		self.tld = self.domain.split(".")[-1]
 
-		self.whoisServers = {}
-		f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "whois-servers.conf"), "r")
-		exec("self.whoisServers = %s"%(f.read()))
+		self.currPath = os.path.dirname(os.path.realpath(__file__))
+		self.tldPath = os.path.join(self.currPath, "tlds")
+		self.tldList = os.listdir(self.tldPath)
 
 	def chooseServer(self):
-		if self.whoisServers.has_key(self.tld):
-			return self.whoisServers[self.tld]
+		if self.tld in self.tldList:
+			settings = {}
+			execfile(os.path.join(self.tldPath, self.tld), {}, settings)
+			if "server" in settings:
+				return settings["server"]
+			else:
+				return self.tld + ".whois-servers.net"
+
 		else:
-			return self.tld + ".whois-servers.net"
+			return self.tld + ".whois-servers.net" 
+
 
 	def run(self, redirect=True):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.whoisServer = self.chooseServer()
 		try:
 			s.connect((self.whoisServer, 43))
 		except:
