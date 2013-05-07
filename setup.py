c6c48f56eb682a6491b19ef5541911c69afaf6c9
Larry Kim <admin@relip.org>
2013-05-07 20:42:06 +0900

    Proper setup script

--- setup.py
+++ setup.py
@@ -7,23 +7,20 @@ import sys, os
 __version__ = '0.1'
 
 setup(name='whois',
-      version=__version__,
-      description="",
-      long_description="",
-      classifiers=[], 
-      keywords='whois',
-      author='Larry Kim',
-      author_email='admin@relip.org',
-      url='http://github.com/relip/python-whois',
-      license='MIT',
-	package_dir={},
-      packages=['whois'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+	version=__version__,
+	description="The Whois client, which is developed in Python language, retrieves domain information from the server.",
+	classifiers=[
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: POSIX",
+		"Environment :: Console",
+		"Programming Language :: Python",
+		"Topic :: Internet",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+	], 
+	keywords='whois',
+	author='Larry Kim',
+	author_email='admin@relip.org',
+	url='http://github.com/relip/python-whois',
+	license='MIT',
+	packages=['whois'],
 )
--- whois/__init__.py
+++ whois/__init__.py
@@ -6,6 +6,6 @@
 # \ \_\\ \_\\"\_\\ \_\  \ \_\ 
 #  \/_/ \/_/ \/_/ \/_/   \/_/ 
 
-from whois import *
-from parser import * 
+from whois import Whois as whois 
+from parser import Parser as parser
 
